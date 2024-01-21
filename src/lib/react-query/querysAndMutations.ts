8b4935c773c09623b9370f714a88823fbb2336d5
Kyryloloshka <kirill.contrmptuous@gmail.com>
2024-01-21 18:32:00 +0200

    fix lastage.ducuments can be undefined

--- src/lib/react-query/querysAndMutations.ts
+++ src/lib/react-query/querysAndMutations.ts
@@ -164,10 +164,10 @@ export const useGetPosts = () => {
     queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
     queryFn: getInfinitePosts as any,
     getNextPageParam: (lastPage: any) => {
-      if (lastPage && lastPage?.documents.length === 0) {
+      if (lastPage && lastPage.documents.length === 0) {
         return null
       }
-      const lastId = lastPage.documents[lastPage?.documents.length - 1].$id;
+      const lastId = lastPage.documents[lastPage.documents.length - 1].$id;
       return lastId
     }
   })
